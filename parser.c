25ffb0522ff17bbd8ca0a9eaeb04fc69320160c9
Suhas44 <suhas8@outlook.com>
2022-07-11 19:47:30 -0500

    Added Slicing

--- parser.c
+++ parser.c
@@ -2,31 +2,42 @@
 #include <stdlib.h>
 #include <string.h>
 
+void parse(char* json, int length);
 char* read_file(char* filename);
-void parse(char* json);
 char* trim(char* json);
+void slice(const char* str, char* result, size_t start, size_t end);
 
 void main() {
     char* json = read_file("info.json");
     json = trim(json);
+    parse(json, strlen(json));
 }
 
-void parse(char* json) {
+void parse(char* json, int length) {
+    if (json[0] != '{' || json[length - 1] != '}') {
+        printf("Error: Expected '{' and '}'\n");
+        return;
+    }
+    char* objects = (char*)malloc(sizeof(char) * (length - 2));
+    slice(json, objects, 1, length - 1);
+}
+
+void object_parse(char* json, int length) {
+    int index = 0;
 }
 
 char* read_file(char* filename) {
     FILE* file = fopen(filename, "r");
-    char* buffer = (char*)calloc(3000, sizeof(char));
+    char* buffer = (char*)malloc(1);
     char ch;
     int i = 0;
-    do {
-        ch = fgetc(file);
+    while ((ch = fgetc(file)) != EOF) {
+        buffer = (char*)realloc(buffer, sizeof(char) * (i + 1));
         buffer[i] = ch;
         i++;
-    } while (ch != EOF);
+    }
     fclose(file);
     return buffer;
-    free(buffer);
 }
 
 char* trim(char* json) {
@@ -34,7 +45,7 @@ char* trim(char* json) {
     int i;
     int j = 0;
     for (i = 0; i < strlen(json); i++) {
-        if (json[i] == 32) {
+        if (json[i] == 32 || json[i] == 10 || json[i] == 13) {
             j++;
         } else {
             buffer[i - j] = json[i];
@@ -42,5 +53,14 @@ char* trim(char* json) {
     }
     char* trim;
     strncpy(trim, buffer, (i - j));
+    free(buffer);
     return trim;
+}
+
+void slice(const char* str, char* result, size_t start, size_t end) {
+    size_t i;
+    for (i = start; i < end; i++) {
+        result[i - start] = str[i];
+    }
+    result[i - start] = '\0';
 }
